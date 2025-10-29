e3322dec75aaa3165028dc4a5556f76dc066a3a4
Andrew Knoesen <andrew@spatialedge.co.za>
2025-10-29 21:33:10 +0200

    bash scripts

--- bin/view_postgres_details.sh
+++ bin/view_postgres_details.sh
@@ -1,3 +1,8 @@
 #!/bin/bash/
 
-kubectl get service postgres-lb -n game-rules-chatbot
+kubectl get secret -n game-rules-chatbot | grep basic-auth
+
+# Get credentials
+kubectl get secret game-rules-chatbot-postgres-app  -n game-rules-chatbot -o jsonpath='{.data.username}' | base64 -d
+kubectl get secret game-rules-chatbot-postgres-app  -n game-rules-chatbot -o jsonpath='{.data.password}' | base64 -d
+
